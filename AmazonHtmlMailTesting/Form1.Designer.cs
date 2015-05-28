3a70fe838ca3d1133d755974ba5f32fe8eda51b3
dcumin39 <cumings@gmail.com>
2015-05-27 22:20:52 -0400

    Initial Commit
    Simple Form for adding To Email Address,Subject Body.
    Performs CSS Inlining using Premailer
    Sends via Amazon SES

---
+++ AmazonHtmlMailTesting/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace AmazonHtmlMailTesting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label2;
+            this.ButtonSend = new System.Windows.Forms.Button();
+            this.TextBoxEmailAddresses = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TextBoxSubject = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.TextBoxHtml = new System.Windows.Forms.RichTextBox();
+            label2 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ButtonSend
+            // 
+            this.ButtonSend.Location = new System.Drawing.Point(917, 368);
+            this.ButtonSend.Name = "ButtonSend";
+            this.ButtonSend.Size = new System.Drawing.Size(75, 23);
+            this.ButtonSend.TabIndex = 0;
+            this.ButtonSend.Text = "Send";
+            this.ButtonSend.UseVisualStyleBackColor = true;
+            this.ButtonSend.Click += new System.EventHandler(this.ButtonSend_Click);
+            // 
+            // TextBoxEmailAddresses
+            // 
+            this.TextBoxEmailAddresses.Location = new System.Drawing.Point(53, 12);
+            this.TextBoxEmailAddresses.Name = "TextBoxEmailAddresses";
+            this.TextBoxEmailAddresses.Size = new System.Drawing.Size(476, 20);
+            this.TextBoxEmailAddresses.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Email";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(12, 41);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(28, 13);
+            label2.TabIndex = 4;
+            label2.Text = "Html";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(539, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Subject";
+            // 
+            // TextBoxSubject
+            // 
+            this.TextBoxSubject.Location = new System.Drawing.Point(588, 12);
+            this.TextBoxSubject.Name = "TextBoxSubject";
+            this.TextBoxSubject.Size = new System.Drawing.Size(404, 20);
+            this.TextBoxSubject.TabIndex = 5;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 381);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1004, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "StatusStrip";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // TextBoxHtml
+            // 
+            this.TextBoxHtml.Location = new System.Drawing.Point(15, 57);
+            this.TextBoxHtml.Name = "TextBoxHtml";
+            this.TextBoxHtml.Size = new System.Drawing.Size(977, 296);
+            this.TextBoxHtml.TabIndex = 8;
+            this.TextBoxHtml.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 403);
+            this.Controls.Add(this.TextBoxHtml);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TextBoxSubject);
+            this.Controls.Add(label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBoxEmailAddresses);
+            this.Controls.Add(this.ButtonSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonSend;
+        private System.Windows.Forms.TextBox TextBoxEmailAddresses;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TextBoxSubject;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.RichTextBox TextBoxHtml;
+    }
+}
+
---
+++ AmazonHtmlMailTesting/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using Amazon.Runtime;
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+using Message = System.Windows.Forms.Message;
+
+namespace AmazonHtmlMailTesting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSend_Click(object sender, EventArgs e)
+        {
+            var FromSender = ConfigurationSettings.AppSettings["FromSender"];
+            var To = TextBoxEmailAddresses.Text;
+
+             var Subject = TextBoxSubject.Text;
+            var Body =  PreMailer.Net.PreMailer.MoveCssInline(TextBoxHtml.Text).Html;
+            // Construct an object to contain the recipient address.
+            Destination destination = new Destination();
+            destination.ToAddresses = (new List<string>() { To });
+
+            // Create the subject and body of the message.
+            Content subject = new Content(Subject);
+            Content textBody = new Content(Body);
+            Body body = new Body();
+            body.Html = textBody;
+
+            // Create a message with the specified subject and body.
+            Amazon.SimpleEmail.Model.Message message = new Amazon.SimpleEmail.Model.Message(subject, body);
+            
+            // Assemble the email.
+            SendEmailRequest request = new SendEmailRequest(FromSender, destination, message);
+            
+            // Choose the AWS region of the Amazon SES endpoint you want to connect to. Note that your sandbox 
+            // status, sending limits, and Amazon SES identity-related settings are specific to a given 
+            // AWS region, so be sure to select an AWS region in which you set up Amazon SES. Here, we are using 
+            // the US West (Oregon) region. Examples of other regions that Amazon SES supports are USEast1 
+            // and EUWest1. For a complete list, see http://docs.aws.amazon.com/ses/latest/DeveloperGuide/regions.html 
+            Amazon.RegionEndpoint REGION = Amazon.RegionEndpoint.USEast1;
+ 
+           
+           
+            // Instantiate an Amazon SES client, which will make the service call.
+            AWSCredentials credentials = new BasicAWSCredentials(ConfigurationSettings.AppSettings["awsAccessKeyId"], ConfigurationSettings.AppSettings["awsSecretAccessKey"]);
+            AmazonSimpleEmailServiceClient client = new AmazonSimpleEmailServiceClient(credentials,REGION);
+           
+            // Send the email.
+            try
+            {
+                toolStripStatusLabel1.Text = "Attempting to send an email through Amazon SES by using the AWS SDK for .NET...";
+                statusStrip1.Refresh();
+                client.SendEmail(request);
+                toolStripStatusLabel1.Text = "Email sent!";
+            }
+            catch (Exception ex)
+            {
+                
+                toolStripStatusLabel1.Text = "Not Sent.  Error message: " + ex.Message;
+                statusStrip1.Refresh();
+            }
+
+
+            toolStripStatusLabel1.Text = "Email sent succesfully!";
+            statusStrip1.Refresh();
+            
+        }
+
+      
+    }
+}
