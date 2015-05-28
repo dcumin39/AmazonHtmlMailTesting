e0b5c59d041d3c614ccad714799d450d87c0abd9
dcumin39 <cumings@gmail.com>
2015-05-28 01:45:58 -0400

    Attempted to preview the message file
    Came out completely unformatted.

--- AmazonHtmlMailTesting/Form1.Designer.cs
+++ AmazonHtmlMailTesting/Form1.Designer.cs
@@ -29,140 +29,205 @@
         private void InitializeComponent()
         {
             System.Windows.Forms.Label label2;
-            this.ButtonSend = new System.Windows.Forms.Button();
-            this.TextBoxEmailAddresses = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.label3 = new System.Windows.Forms.Label();
             this.TextBoxSubject = new System.Windows.Forms.TextBox();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextBoxEmailAddresses = new System.Windows.Forms.TextBox();
+            this.ButtonSend = new System.Windows.Forms.Button();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.TextBoxHtml = new System.Windows.Forms.RichTextBox();
             this.checkBoxValidateOutlook2007 = new System.Windows.Forms.CheckBox();
+            this.TextBoxHtml = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.TextBoxPreview = new System.Windows.Forms.RichTextBox();
+            this.previewButton = new System.Windows.Forms.Button();
             label2 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label2
+            // tabControl1
             // 
-            label2.AutoSize = true;
-            label2.Location = new System.Drawing.Point(12, 41);
-            label2.Name = "label2";
-            label2.Size = new System.Drawing.Size(28, 13);
-            label2.TabIndex = 4;
-            label2.Text = "Html";
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(-4, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1010, 431);
+            this.tabControl1.TabIndex = 0;
             // 
-            // ButtonSend
+            // tabPage1
             // 
-            this.ButtonSend.Location = new System.Drawing.Point(917, 368);
-            this.ButtonSend.Name = "ButtonSend";
-            this.ButtonSend.Size = new System.Drawing.Size(75, 23);
-            this.ButtonSend.TabIndex = 0;
-            this.ButtonSend.Text = "Send";
-            this.ButtonSend.UseVisualStyleBackColor = true;
-            this.ButtonSend.Click += new System.EventHandler(this.ButtonSend_Click);
+            this.tabPage1.Controls.Add(this.previewButton);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.TextBoxSubject);
+            this.tabPage1.Controls.Add(label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.TextBoxEmailAddresses);
+            this.tabPage1.Controls.Add(this.ButtonSend);
+            this.tabPage1.Controls.Add(this.checkBoxValidateOutlook2007);
+            this.tabPage1.Controls.Add(this.TextBoxHtml);
+            this.tabPage1.Controls.Add(this.statusStrip1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(1002, 405);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // TextBoxEmailAddresses
+            // tabPage2
             // 
-            this.TextBoxEmailAddresses.Location = new System.Drawing.Point(53, 12);
-            this.TextBoxEmailAddresses.Name = "TextBoxEmailAddresses";
-            this.TextBoxEmailAddresses.Size = new System.Drawing.Size(476, 20);
-            this.TextBoxEmailAddresses.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Email";
+            this.tabPage2.Controls.Add(this.TextBoxPreview);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(1002, 405);
+            this.tabPage2.TabIndex = 0;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(539, 15);
+            this.label3.Location = new System.Drawing.Point(539, 9);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(43, 13);
-            this.label3.TabIndex = 6;
+            this.label3.TabIndex = 15;
             this.label3.Text = "Subject";
             // 
             // TextBoxSubject
             // 
-            this.TextBoxSubject.Location = new System.Drawing.Point(588, 12);
+            this.TextBoxSubject.Location = new System.Drawing.Point(588, 6);
             this.TextBoxSubject.Name = "TextBoxSubject";
             this.TextBoxSubject.Size = new System.Drawing.Size(404, 20);
-            this.TextBoxSubject.TabIndex = 5;
+            this.TextBoxSubject.TabIndex = 14;
             // 
-            // statusStrip1
+            // label2
             // 
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 417);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(1004, 22);
-            this.statusStrip1.TabIndex = 7;
-            this.statusStrip1.Text = "StatusStrip";
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(12, 35);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(28, 13);
+            label2.TabIndex = 13;
+            label2.Text = "Html";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Email";
+            // 
+            // TextBoxEmailAddresses
+            // 
+            this.TextBoxEmailAddresses.Location = new System.Drawing.Point(53, 6);
+            this.TextBoxEmailAddresses.Name = "TextBoxEmailAddresses";
+            this.TextBoxEmailAddresses.Size = new System.Drawing.Size(476, 20);
+            this.TextBoxEmailAddresses.TabIndex = 11;
+            // 
+            // ButtonSend
+            // 
+            this.ButtonSend.Location = new System.Drawing.Point(914, 353);
+            this.ButtonSend.Name = "ButtonSend";
+            this.ButtonSend.Size = new System.Drawing.Size(75, 23);
+            this.ButtonSend.TabIndex = 10;
+            this.ButtonSend.Text = "Send";
+            this.ButtonSend.UseVisualStyleBackColor = true;
             // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
             this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
             // 
-            // TextBoxHtml
-            // 
-            this.TextBoxHtml.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.TextBoxHtml.Location = new System.Drawing.Point(15, 57);
-            this.TextBoxHtml.Name = "TextBoxHtml";
-            this.TextBoxHtml.Size = new System.Drawing.Size(977, 296);
-            this.TextBoxHtml.TabIndex = 8;
-            this.TextBoxHtml.Text = "";
-            // 
             // checkBoxValidateOutlook2007
             // 
             this.checkBoxValidateOutlook2007.AutoSize = true;
             this.checkBoxValidateOutlook2007.Checked = true;
             this.checkBoxValidateOutlook2007.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBoxValidateOutlook2007.Location = new System.Drawing.Point(734, 374);
+            this.checkBoxValidateOutlook2007.Location = new System.Drawing.Point(678, 357);
             this.checkBoxValidateOutlook2007.Name = "checkBoxValidateOutlook2007";
             this.checkBoxValidateOutlook2007.Size = new System.Drawing.Size(149, 17);
-            this.checkBoxValidateOutlook2007.TabIndex = 9;
+            this.checkBoxValidateOutlook2007.TabIndex = 18;
             this.checkBoxValidateOutlook2007.Text = "Validate For Outlook 2007";
             this.checkBoxValidateOutlook2007.UseVisualStyleBackColor = true;
+            // 
+            // TextBoxHtml
+            // 
+            this.TextBoxHtml.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.TextBoxHtml.Location = new System.Drawing.Point(15, 51);
+            this.TextBoxHtml.Name = "TextBoxHtml";
+            this.TextBoxHtml.Size = new System.Drawing.Size(977, 296);
+            this.TextBoxHtml.TabIndex = 17;
+            this.TextBoxHtml.Text = "";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 383);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1002, 22);
+            this.statusStrip1.TabIndex = 16;
+            this.statusStrip1.Text = "StatusStrip";
+            // 
+            // TextBoxPreview
+            // 
+            this.TextBoxPreview.Location = new System.Drawing.Point(0, 3);
+            this.TextBoxPreview.Name = "TextBoxPreview";
+            this.TextBoxPreview.Size = new System.Drawing.Size(999, 399);
+            this.TextBoxPreview.TabIndex = 0;
+            this.TextBoxPreview.Text = "";
+            // 
+            // previewButton
+            // 
+            this.previewButton.Location = new System.Drawing.Point(833, 353);
+            this.previewButton.Name = "previewButton";
+            this.previewButton.Size = new System.Drawing.Size(75, 23);
+            this.previewButton.TabIndex = 19;
+            this.previewButton.Text = "Preview";
+            this.previewButton.UseVisualStyleBackColor = true;
+            this.previewButton.Click += new System.EventHandler(this.previewButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1004, 439);
-            this.Controls.Add(this.checkBoxValidateOutlook2007);
-            this.Controls.Add(this.TextBoxHtml);
-            this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.TextBoxSubject);
-            this.Controls.Add(label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.TextBoxEmailAddresses);
-            this.Controls.Add(this.ButtonSend);
+            this.ClientSize = new System.Drawing.Size(1012, 456);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button ButtonSend;
-        private System.Windows.Forms.TextBox TextBoxEmailAddresses;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        public System.Windows.Forms.Button previewButton;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox TextBoxSubject;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TextBoxEmailAddresses;
+        private System.Windows.Forms.Button ButtonSend;
+        private System.Windows.Forms.CheckBox checkBoxValidateOutlook2007;
+        private System.Windows.Forms.RichTextBox TextBoxHtml;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
-        private System.Windows.Forms.RichTextBox TextBoxHtml;
-        private System.Windows.Forms.CheckBox checkBoxValidateOutlook2007;
+        private System.Windows.Forms.RichTextBox TextBoxPreview;
+
     }
 }
 
--- AmazonHtmlMailTesting/Form1.cs
+++ AmazonHtmlMailTesting/Form1.cs
@@ -8,9 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Configuration;
+using System.IO;
+using System.Net.Mail;
 using Amazon.Runtime;
 using Amazon.SimpleEmail;
 using Amazon.SimpleEmail.Model;
+using MailBee.Outlook;
 using Message = System.Windows.Forms.Message;
 
 namespace AmazonHtmlMailTesting
@@ -99,50 +102,58 @@ namespace AmazonHtmlMailTesting
         "z-index:"
         };
 
+        public class EmailParamsFromForm
+        {
+            public string FromSender { get; set; }
+
+            public string To { get; set; }
+
+            public string Subject { get; set; }
+
+            public string Body { get; set; }
+
+        }
         private void ButtonSend_Click(object sender, EventArgs e)
         {
-            var FromSender = ConfigurationSettings.AppSettings["FromSender"];
-            var To = TextBoxEmailAddresses.Text;
 
-             var Subject = TextBoxSubject.Text;
 
-            var text = TextBoxHtml.Text;
-            if (checkBoxValidateOutlook2007.Checked)
+          
+
+            var emailParameters = GetEmailParamsFromForm();
+            if (emailParameters == null)
             {
-                if (TestForValidOutLook2007(text)) return;
+                return;
             }
-
-            var Body =  PreMailer.Net.PreMailer.MoveCssInline(text).Html;
 
             // Construct an object to contain the recipient address.
             Destination destination = new Destination();
-            destination.ToAddresses = (new List<string>() { To });
+            destination.ToAddresses = (new List<string>() { emailParameters.To });
 
             // Create the subject and body of the message.
-            Content subject = new Content(Subject);
-            Content textBody = new Content(Body);
+            Content subject = new Content(emailParameters.Subject);
+            Content textBody = new Content(emailParameters.Body);
             Body body = new Body();
             body.Html = textBody;
 
             // Create a message with the specified subject and body.
             Amazon.SimpleEmail.Model.Message message = new Amazon.SimpleEmail.Model.Message(subject, body);
-            
+
             // Assemble the email.
-            SendEmailRequest request = new SendEmailRequest(FromSender, destination, message);
-            
+            SendEmailRequest request = new SendEmailRequest(emailParameters.FromSender, destination, message);
+
             // Choose the AWS region of the Amazon SES endpoint you want to connect to. Note that your sandbox 
             // status, sending limits, and Amazon SES identity-related settings are specific to a given 
             // AWS region, so be sure to select an AWS region in which you set up Amazon SES. Here, we are using 
             // the US West (Oregon) region. Examples of other regions that Amazon SES supports are USEast1 
             // and EUWest1. For a complete list, see http://docs.aws.amazon.com/ses/latest/DeveloperGuide/regions.html 
             Amazon.RegionEndpoint REGION = Amazon.RegionEndpoint.USEast1;
- 
-           
-           
+
+
+
             // Instantiate an Amazon SES client, which will make the service call.
             AWSCredentials credentials = new BasicAWSCredentials(ConfigurationSettings.AppSettings["awsAccessKeyId"], ConfigurationSettings.AppSettings["awsSecretAccessKey"]);
-            AmazonSimpleEmailServiceClient client = new AmazonSimpleEmailServiceClient(credentials,REGION);
-           
+            AmazonSimpleEmailServiceClient client = new AmazonSimpleEmailServiceClient(credentials, REGION);
+
             // Send the email.
             try
             {
@@ -153,7 +164,7 @@ namespace AmazonHtmlMailTesting
             }
             catch (Exception ex)
             {
-                
+
                 toolStripStatusLabel1.Text = "Not Sent.  Error message: " + ex.Message;
                 statusStrip1.Refresh();
             }
@@ -161,7 +172,25 @@ namespace AmazonHtmlMailTesting
 
             toolStripStatusLabel1.Text = "Email sent succesfully!";
             statusStrip1.Refresh();
-            
+
+
+        }
+
+        private EmailParamsFromForm GetEmailParamsFromForm( )
+        {
+            var text = TextBoxHtml.Text;
+            if (checkBoxValidateOutlook2007.Checked)
+            {
+                if (TestForValidOutLook2007(text)) return null;
+            }
+
+            return new EmailParamsFromForm()
+            {
+                Body = PreMailer.Net.PreMailer.MoveCssInline(text).Html,
+                Subject = TextBoxSubject.Text,
+                To = TextBoxEmailAddresses.Text,
+                FromSender = ConfigurationSettings.AppSettings["FromSender"]
+            };
         }
 
         private bool TestForValidOutLook2007(string text)
@@ -184,6 +213,33 @@ namespace AmazonHtmlMailTesting
                 }
             }
             return false;
+        }
+
+        private void previewButton_Click(object sender, EventArgs e)
+        {
+
+            var emailParameters = GetEmailParamsFromForm();
+            if (emailParameters == null)
+            {
+                return;
+            }
+            var outLookMessage = new MailBee.Mime.MailMessage();
+            outLookMessage.BodyHtmlText = emailParameters.Body;
+            outLookMessage.From.Email = emailParameters.FromSender;
+            outLookMessage.From.DisplayName = emailParameters.FromSender;
+            outLookMessage.To.Add("Test Email", emailParameters.To);
+            outLookMessage.Subject = emailParameters.Subject;
+            MsgConvert conv = new MsgConvert();
+            conv.MsgAsUnicode = true;
+            conv.HtmlToRtfMethod = HtmlToRtfConversionMethod.Internal;
+            conv.MsgAsDraft = false;
+            var cTempResultMsg = "C:\\Temp\\result.msg";
+            conv.MailMessageToMsg(outLookMessage, cTempResultMsg);
+            MsgConvert convertToBodyHtml = new MsgConvert();
+            var BodyHtmlMessage = convertToBodyHtml.MsgToMailMessage(cTempResultMsg);
+             OutlookStorage.Message outlookMsg = new OutlookStorage.Message(cTempResultMsg);
+             TextBoxPreview.Rtf = outlookMsg.BodyRTF;
+
         }
     }
 }
